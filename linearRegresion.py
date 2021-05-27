62b663233584970f25739f8b3c4a5efeb65caac9
vishaltanna6 <70106580+vishaltanna6@users.noreply.github.com>
2021-05-27 19:15:34 +0530

    Linear Regression code

---
+++ linearRegresion.py
@@ -0,0 +1,30 @@
+#importing pandas 
+import pandas as pd
+
+#reading the dataset from SalarayData.csv 
+dataset = pd.read_csv("SalaryData.csv")
+
+#getting the dataset colums
+print(dataset.columns)
+
+X = dataset['YearsExperience']
+y = dataset['Salary']
+
+#importing LinearRegression 
+from sklearn.linear_model import LinearRegression
+
+#Creating linear regression model
+model = LinearRegression()
+
+import numpy as np
+X = np.array(X).reshape(-1,1)
+
+#dividing the data into train and test split
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split (X, y, test_size=0.2, random_state=42)
+
+#fitting data into the model.
+model.fit(X_train,y_train)
+
+#predict the output
+print(model.predict([[1]]))
